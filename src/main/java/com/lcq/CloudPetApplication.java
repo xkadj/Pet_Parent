730f72fc47590da050fd3f5153fd6dcb9f4b7d45
xkadj <1605520438@qq.com>
2020-11-25 19:19:20 +0800

    一款乐宠APP

---
+++ src/main/java/com/lcq/CloudPetApplication.java
@@ -0,0 +1,13 @@
+package com.lcq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudPetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lcq/CloudPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
